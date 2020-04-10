cfa42d59d1a58e84e969d0433a7489dd1270c603
Logan Zartman <logan.zartman@utexas.edu>
2020-04-10 16:43:14 -0500

    logan's bespoke parameters

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -3,15 +3,15 @@ init_x = 14.7
 init_y = 14.24
 init_r = 0
 
-k1 = 0.30 -- tangent translation error from translation 
+k1 = 0.70 -- tangent translation error from translation 
 k2 = 0.05 -- tangent translation error from rotation
 k3 = 0.01 -- normal translation error from translation
 k4 = 0.05 -- normal translation error from rotation
-k5 = 0.05 -- rotation error from translation
-k6 = 0.1  -- rotation error from rotation
+k5 = 0.30 -- rotation error from translation
+k6 = 0.70 -- rotation error from rotation
 
 correlation = 0.9
-sigma = 4
+sigma = 2
 d_long = .75
 d_short = .75
 s_max_offset = 1.0
