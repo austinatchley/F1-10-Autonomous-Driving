fcded53cf5475d0ce9ec2a458d0eb0a6a70fa984
Austin Atchley <austinatchley@gmail.com>
2020-05-11 01:58:55 -0500

    Tweak rrt params

--- config/navigation.lua
+++ config/navigation.lua
@@ -2,8 +2,8 @@
 rrt_max_iter_frame = 3000 -- maximum iterations per frame
 rrt_extra_iter = 6000 -- minimum total iterations before stopping
 rrt_max_iter = 60000 -- total max iterations before giving up
-rrt_goal_tolerance = 2 -- meters
-rrt_wall_dilation = 0.15 -- meters
+rrt_goal_tolerance = 1.75 -- meters
+rrt_wall_dilation = 0.2 -- meters
 rrt_steering_eta = 1.0 -- meters
 
 rrt_neighborhood_radius = 1.5 -- meters
