6cdfe25e5d385ab9a62561ab42ffba7bac48039c
Logan Zartman <logan.zartman@utexas.edu>
2020-05-11 00:23:51 -0500

    switch to gdc2 and tune params

--- config/navigation.lua
+++ config/navigation.lua
@@ -1,8 +1,8 @@
 -- RRT/RRT* pathfinding parameters --
 rrt_max_iter_frame = 3000 -- maximum iterations per frame
-rrt_extra_iter = 1000 -- minimum total iterations before stopping
-rrt_max_iter = 50000 -- total max iterations before giving up
-rrt_goal_tolerance = 0.5 -- meters (set less than 2*wall_dilation for safety)
+rrt_extra_iter = 6000 -- minimum total iterations before stopping
+rrt_max_iter = 40000 -- total max iterations before giving up
+rrt_goal_tolerance = 2 -- meters (set less than 2*wall_dilation for safety)
 rrt_wall_dilation = 0.15 -- meters
 rrt_neighborhood_radius = 1.5 -- meters
 rrt_neighbor_count = 5 -- number of neighbors for nearest neighbors
